93f894946936b45c204f68c308b26be3ad35340c
Kevin Xiang <kxiang644@gmail.com>
2025-03-11 19:41:01 -0400

    Finally loaded the initial tree

--- CANStandardItem.py
+++ CANStandardItem.py
@@ -2,41 +2,41 @@ from PyQt5.QtGui import QStandardItem
 
 class CANStandardItem(QStandardItem):
     def __init__(self, text, message, binding):
-        super().__init__(text)
+        super().__init__(str(text))
         self.message = message
         self.binding = binding
     
-    # def setData(self, value, role):
-    #     info = self.message.mappings[self.binding]
-    #     print("Updating to " + value)
-    #     try:
-    #         info["function"](int(value))
-    #     except Exception as e:
-    #         print("this wus a bad output!!!")
-    #         return
+    def setData(self, value, role):
+        info = self.message.mappings[self.binding]
+        print("Updating to " + value)
+        try:
+            info["function"](int(value))
+        except Exception as e:
+            print(e)
+            return
             
 
-    #     super().setData(value, role)
+        super().setData(value, role)
 
-    def setData(self, value, role):
-        try:
-            if hasattr(self.message, "mappings") and self.binding in self.message.mappings:
-                info = self.message.mappings[self.binding]
-                print(f"Updating {self.binding} to {value}")
-                try:
-                    info["function"](int(value))
-                except Exception as e:
-                    print("Invalid input:", e)
-                    return
-            else:
-                print(f"Warning: '{self.binding}' not found in mappings of {type(self.message).__name__}")
-                if hasattr(self.message, "mappings"):
-                    print(f"Available mappings: {list(self.message.mappings.keys())}")
-                else:
-                    print(f"{type(self.message).__name__} does not have a mappings attribute.")
+    # def setData(self, value, role):
+    #     try:
+    #         if hasattr(self.message, "mappings") and self.binding in self.message.mappings:
+    #             info = self.message.mappings[self.binding]
+    #             print(f"Updating {self.binding} to {value}")
+    #             try:
+    #                 info["function"](int(value))
+    #             except Exception as e:
+    #                 print("Invalid input:", e)
+    #                 return
+    #         else:
+    #             print(f"Warning: '{self.binding}' not found in mappings of {type(self.message).__name__}")
+    #             if hasattr(self.message, "mappings"):
+    #                 print(f"Available mappings: {list(self.message.mappings.keys())}")
+    #             else:
+    #                 print(f"{type(self.message).__name__} does not have a mappings attribute.")
 
-                return
+    #             return
 
-            super().setData(value, role)
-        except Exception as e:
-            print(f"Error in setData: {e}")
+    #         super().setData(value, role)
+    #     except Exception as e:
+    #         print(f"Error in setData: {e}")
